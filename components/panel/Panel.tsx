a8bbcd19d3e033ae5c42c4d40a24f20a611107d8
Oskwame <nanaqwamee124@gmail.com>
2025-01-06 14:10:40 +0000

    New Update V1.01

---
+++ components/panel/Panel.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaCogs, FaCalendarAlt, FaBell } from 'react-icons/fa';
+
+const Panel: React.FC = () => {
+  return (
+    <aside className="w-48 bg-gray-100 h-full fixed border-2 border-blue-400">
+      <nav className="py-6 mt-10">
+        <ul>
+          <li>
+            <a href="/bookedAppointment" className="flex items-center px-3 py-3 hover:bg-blue-400">
+              <FaCalendarAlt className="mr-3 text-xl" />
+              Booked Appointments
+            </a>
+          </li>
+          <li>
+            <a href="/manageServices" className="flex items-center px-3 py-3 hover:bg-blue-400">
+              <FaCogs className="mr-3 text-xl" />
+              Manage Services
+            </a>
+          </li>
+          <li>
+            <a href="/alerts" className="flex items-center px-3 py-3 hover:bg-blue-400">
+              <FaBell className="mr-3 text-xl" />
+              Alerts
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Panel;
